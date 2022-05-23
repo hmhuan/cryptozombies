904486c870fa9a674314135c846f9a09d13b4d6b
Minh-Huan Huynh <1612858@student.hcmus.edu.vn>
2022-05-24 00:24:34 +0900

    use interface

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -18,10 +18,20 @@ contract KittyInterface {
 }
 
 contract ZombieFeeding is ZombieFactory {
+
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(ckAddress);
+
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
         uint newDna = ((_targetDna % dnaModulus + myZombie.dna) / 2) % dnaModulus;
         _createZombie("RandomName", newDna);
+    }
+
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint kittyDna;
+        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna);
     }
 }
