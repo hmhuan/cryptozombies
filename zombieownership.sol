940f98df5e02367236562d77c1b77d05fc710689
Minh-Huan Huynh <1612858@student.hcmus.edu.vn>
2022-06-23 21:56:47 +0900

    add private transfer function

--- zombieownership.sol
+++ zombieownership.sol
@@ -12,4 +12,20 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     return zombieToOwner[_tokenId];
   }
   
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
+    ownerZombieCount[_to]++;
+    ownerZombieCount[_from]--;
+    zombieToOwner[_tokenId] = _to;
+    emit Transfer(_from, _to, _tokenId);
+  }
+
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+    // 2. Add the require statement here
+    // 3. Call _transfer
+  }
+
+  function approve(address _approved, uint256 _tokenId) external payable {
+
+  }
+  
 }
